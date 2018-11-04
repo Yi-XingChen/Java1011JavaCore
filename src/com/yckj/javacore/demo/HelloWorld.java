67a8adbceeb3910f797a0400167827c3d0093afd
chinazhojy <918943180@qq.com>
2018-11-04 17:27:04 +0800

    Java1011班Java SE学习专用项目创建

---
+++ src/com/yckj/javacore/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.yckj.javacore.demo;
+
+/**
+ * CopyRight (C) 2018 英才汇硕信息技术有限公司(YCKJ) All rights Reserved
+ *
+ * HelloWorld程序
+ *
+ * @author zhoujy
+ * @version 1.0
+ * @date 20181104
+ */
+public class HelloWorld {
+    /**
+     * 主函数
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
